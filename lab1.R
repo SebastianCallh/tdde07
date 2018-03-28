9802d08a40ef9e43f7579b63e59b87609b7da634
SebastianCallh <sebastian.callh@gmail.com>
2018-03-28 16:45:51 +0200

    work during lab, probably only assignment 3 left

--- lab1.R
+++ lab1.R
@@ -1,5 +1,5 @@
 # 1. Bernoulli ... again.
-library(geoR)
+
 # a) Show that posterior converges to 14/20 approx. 0.7
 alpha0 <- 2
 beta0  <- 2 
@@ -8,21 +8,17 @@ n      <- 20
 s      <- 14
 f      <- n - s
 post   <- rbeta(nDraws, alpha0 + s, beta0 + f)
-hist(post)
+hist(post, breaks = 100)
 
 # b) Simulation to compute posterior probability P(theta < 0.4 | y)
 thetas <- rbeta(nDraws, alpha0 +s , beta0 + f)
 length(thetas[thetas<0.4]) / length(thetas)  # Simulated value
 pbeta(0.4, alpha0+s, beta0+f)                # Exact value
-hist(thetas)
+hist(thetas, breaks = 100)
 
 # c) Log odds posterior distribution
 fi <- sapply(thetas, function(theta) { log(theta / (1 - theta)) })
-hist(fi)  
-
-# Ask question: Did we the fuck do this good? Or right? or somethin?
-
-
+hist(fi, breaks = 100)
 
 # 2. Log-normal distribution and the Gini coefficient.
 incomes <- c(14, 25, 45, 25, 30, 33, 19, 50, 34, 67)
@@ -30,48 +26,62 @@ mu <- 3.5
 n  <- length(incomes)
 tau2 <- sum((log(incomes) - mu)^2) / n 
 
-# a) simulate draws
-# Do the histograms need normalization before any conclusions?
-draws <- rinvchisq(nDraws, n, tau2)
-x     <- seq(0, 3, 0.01)
-hist(draws, breaks = 1000)  # Simulated 
-plot(dinvchisq(x, n, tau2)) # Distribution
+# a) Simulate posterior draws
+X      <- rchisq(nDraws, n)
+sigma2 <- n*tau2/X
+hist(sigma2, breaks = 100)
+
+# Define chi squared in terms of Gamma distribution
+dinvgamma <- function(x, a, b) {
+  (b^a)/gamma(a) * x^(-a-1) * exp(-b/x)
+}
+
+dinvchisq2 <- function(x, n, tau2) {
+  a <- n / 2
+  b <- n*tau2 / 2
+  dinvgamma(x, a, b)
+}
+
+plot(dinvchisq2(seq(0, 3, 0.01), n, tau2), type = 'l') # Distribution
 
 # b) Gini coefficient posterior
-# If one bin contains a value such that the cumsum exceeds i.e. 25 it will still be dropped
-# What does 1st Qu.3rd Qu.do?
-G      <- 2*pnorm(sqrt(draws/2)) - 1
+G      <- 2*pnorm(sqrt(sigma2/2)) - 1
 G.hist <- hist(G, breaks = 1000, xlim = 0:1)
-csum   <- cumsum(G.hist$counts)
-start  <- length(csum[csum < 250])
-end    <- length(csum[csum < 9750])
-shit <- G.hist$counts[start:end]
-plot(G.hist$counts[start:end], type = "h")
 
-# c) G credibility interval
-H <- G / sum(G)
-H.hist <- hist(H, breaks = 1000)
+# c) G Credible interval
+G.slice <- sort(G)[(0.025*nDraws):(0.975*nDraws)]
+cred.lower.bound <- min(G.slice)
+cred.upper.bound <- max(G.slice)
 
-csum <- cumsum(sort(G / sum(G)))
-hist(sort(G / sum(G)))
-start <- length(csum[csum < 0.025])
-end   <- length(csum[csum < 0.975])
+# c) G Highest Posterior Density (HPD)
+G.dens  <- density(G)
+plot(G.dens, type = 'p')
 
-G_prime <- G
-G_prime <- G_prime[-(1:start)]
-hist(G_prime, breaks = 1000, xlim = 0:1)
-#G_prime <- G_prime[-(end:length(G))]
-
-density(G)
+i <- order(G.dens$y, decreasing = TRUE)
+csum <- cumsum(G.dens$y[i])
+m    <- length(csum[csum < sum(G.dens$y)*0.95])
+H    <- G.dens$x[i][1:m]
+hpd.lower.bound <- min(H)
+hpd.upper.bound <- max(H)
 
 # 3
-# Each row in matrix p ~ 1 k value
-y  <- c(-2.44, 2.14, 2.54, 1.83, 2.02, 2.33, -2.79, 2.23, 2.07, 2.02)
-k  <- dexp(seq(0, 5, 0.01))
+# Each row in matrix p ~ 1 value of k, wrong, should be scalar
+Y  <- c(-2.44, 2.14, 2.54, 1.83, 2.02, 2.33, -2.79, 2.23, 2.07, 2.02)
+K  <- dexp(seq(0, 5, 0.01))
 mu <- 2.39
+
+
 Von.Mises <- function (y, mu, k){
   exp(k*cos(y - mu)) / (2*pi*besselI(k, 0))
 }
-p <- sapply(k, function (x) { Von.Mises(y, mu, x) })
-shist <- hist(p, breaks = 100)
-density(p)
+
+likelihood <- function (k) { prod(Von.Mises(Y, mu, k)) }
+posterior  <- function (k) { likelihood(k) * k } #?
+
+plot(K)
+plot(sapply(K, likelihood))
+plot(sapply(K, posterior))
+
+
+plot(Von.Mises(Y, mu, 0), ylim = c(0,4))
+points(Y)
